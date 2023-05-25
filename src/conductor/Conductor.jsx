52805a930d665b069edafdbd43cc1db0392d9112
Samuel Bermudez Valencia <samuel.bermudezv@uqvirtual.edu.co>
2023-05-25 01:23:32 -0500

    5

--- src/conductor/Conductor.jsx
+++ src/conductor/Conductor.jsx
@@ -30,11 +30,12 @@ class Conductor extends Component {
     modalInsertar: false,
     conductorSeleccionado: null,
     loading: false,
-    error: null
+    error: null,
+    mostrarAlerta: false,
+    conductorAEliminar: null,
   };
 
   handleChange = (e) => {
-    // Actualiza el valor de los campos del formulario en el estado
     this.setState({
       form: {
         ...this.state.form,
@@ -44,26 +45,33 @@ class Conductor extends Component {
   }
 
   mostrarModalInsertar = () => {
-    // Muestra el modal de registro de conductor y establece los valores en el formulario
     this.setState({
-      form: {
-        nombre: '',
-        apellidos: '',
-        cedula: '',
-        correo: '',
-        direccion: '',
-        sexo: '',
-        ciudad_id: '',
-        id: ''
-      },
-      modalInsertar: true
+      modalInsertar: true,
     });
   }
 
   ocultarModalInsertar = () => {
-    // Oculta el modal de registro de conductor y reinicia los valores del formulario
     this.setState({
       modalInsertar: false,
+    });
+  }
+
+  mostrarModalEditar = (conductor) => {
+    this.setState({
+      form: conductor,
+      modalInsertar: true,
+    });
+  }
+
+  insertarConductor = () => {
+    const { data, form } = this.state;
+    const newConductor = {
+      ...form,
+      id: data.length + 1,
+    };
+    const newData = [...data, newConductor];
+    this.setState({
+      data: newData,
       form: {
         nombre: '',
         apellidos: '',
@@ -72,73 +80,68 @@ class Conductor extends Component {
         direccion: '',
         sexo: '',
         ciudad_id: '',
-        id: ''
+        id: '',
       },
-      conductorSeleccionado: null,
+      modalInsertar: false,
     });
   }
 
-  eliminarConductor = (id) => {
-    // Elimina el conductor con el id especificado de la lista de conductores
-    const { data } = this.state;
-    const newData = data.filter(conductor => conductor.id !== id);
-    this.setState({ data: newData });
-  }
-
-  editarConductor = (conductor) => {
-    // Establece el conductor seleccionado en el estado y muestra el modal de registro de conductor
-    this.setState({ conductorSeleccionado: conductor }, () => {
-      this.mostrarModalInsertar();
-    });
-  }
-
-  insertar = () => {
-    // Inserta un nuevo conductor en la lista de conductores
-    const { form, data } = this.state;
-    const newConductor = {
-      id: form.id,
-      nombre: form.nombre,
-      apellidos: form.apellidos,
-      cedula: form.cedula,
-      correo: form.correo,
-      direccion: form.direccion,
-      sexo: form.sexo,
-      ciudad_id: form.ciudad_id,
-    };
-    const newData = [...data, newConductor];
-    this.setState({ data: newData, modalInsertar: false });
-  }
-
-  actualizar = () => {
-    // Actualiza los datos del conductor seleccionado en la lista de conductores
-    const { form, data } = this.state;
-    const updatedConductor = {
-      id: form.id,
-      nombre: form.nombre,
-      apellidos: form.apellidos,
-      cedula: form.cedula,
-      correo: form.correo,
-      direccion: form.direccion,
-      sexo: form.sexo,
-      ciudad_id: form.ciudad_id,
-    };
-    const newData = data.map(conductor => {
+  editarConductor = () => {
+    const { data, form } = this.state;
+    const newData = data.map((conductor) => {
       if (conductor.id === form.id) {
-        return updatedConductor;
+        return form;
       }
       return conductor;
     });
-    this.setState({ data: newData, modalInsertar: false });
+    this.setState({
+      data: newData,
+      form: {
+        nombre: '',
+        apellidos: '',
+        cedula: '',
+        correo: '',
+        direccion: '',
+        sexo: '',
+        ciudad_id: '',
+        id: '',
+      },
+      modalInsertar: false,
+    });
+  }
+
+  mostrarModalEliminar = (conductor) => {
+    this.setState({
+      conductorAEliminar: conductor,
+      mostrarAlerta: true,
+    });
+  }
+
+  ocultarModalEliminar = () => {
+    this.setState({
+      conductorAEliminar: null,
+      mostrarAlerta: false,
+    });
+  }
+
+  eliminarConductor = () => {
+    const { data, conductorAEliminar } = this.state;
+    const newData = data.filter((conductor) => conductor.id !== conductorAEliminar.id);
+    this.setState({
+      data: newData,
+      mostrarAlerta: false,
+    });
   }
 
   render() {
+    const { data, form, modalInsertar, mostrarAlerta, conductorAEliminar } = this.state;
+
     return (
-      <>
-        <Container>
-          <br />
+      <Container>
+         <br />
           <h1>
             <div style={{ display: 'flex', alignItems: 'center' }}>
-              <span style={{ marginRight: 'auto' }}>Lista de usuarios</span>
+              <span style={{ marginRight: 'auto' }}>Lista de conductores</span>
               <img src={abeja} alt="abeja" style={{ width: '6%', height: '6%', marginRight: '5px' }} />
               <Button color="success" onClick={() => this.mostrarModalInsertar()} style={{ marginRight: '10px' }}>
                 Registrar conductor
@@ -147,76 +150,111 @@ class Conductor extends Component {
           </h1>
           <br />
           <br />
-          <Table>
-            <thead>
-              <tr>
-                <th style={{ padding: 10 }}>Id</th>
-                <th style={{ padding: 10 }}>Nombre</th>
-                <th style={{ padding: 10 }}>Apellidos</th>
-                <th style={{ padding: 10 }}>Cédula</th>
-                <th style={{ padding: 10 }}>Correo</th>
-                <th style={{ padding: 10 }}>Dirección</th>
-                <th style={{ padding: 10 }}>Sexo</th>
-                <th style={{ padding: 10 }}>Ciudad</th>
+        <Table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Apellidos</th>
+              <th>Cédula</th>
+              <th>Correo</th>
+              <th>Dirección</th>
+              <th>Sexo</th>
+              <th>Ciudad ID</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((conductor) => (
+              <tr key={conductor.id}>
+                <td>{conductor.id}</td>
+                <td>{conductor.nombre}</td>
+                <td>{conductor.apellidos}</td>
+                <td>{conductor.cedula}</td>
+                <td>{conductor.correo}</td>
+                <td>{conductor.direccion}</td>
+                <td>{conductor.sexo}</td>
+                <td>{conductor.ciudad_id}</td>
+                <td>
+                  <Button color="primary" onClick={() => this.mostrarModalEditar(conductor)}>Editar</Button>{' '}
+                  <Button color="danger" onClick={() => this.mostrarModalEliminar(conductor)}>Eliminar</Button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {this.state.data.map((conductor) => (
-                <tr key={conductor.id}>
-                  <td style={{ padding: 10 }}>{conductor.id}</td>
-                  <td style={{ padding: 10 }}>{conductor.nombre}</td>
-                  <td style={{ padding: 10 }}>{conductor.apellidos}</td>
-                  <td style={{ padding: 10 }}>{conductor.cedula}</td>
-                  <td style={{ padding: 10 }}>{conductor.correo}</td>
-                  <td style={{ padding: 10 }}>{conductor.direccion}</td>
-                  <td style={{ padding: 10 }}>{conductor.sexo}</td>
-                  <td style={{ padding: 10 }}>{conductor.ciudad_id}</td>
-                  <td>
-                    <Button color="primary" onClick={() => this.editarConductor(conductor)}>Editar</Button>{" "}
-                    <Button color="danger" onClick={() => this.eliminarConductor(conductor.id)}>Eliminar</Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Container>
+            ))}
+          </tbody>
+        </Table>
 
-        <Modal isOpen={this.state.modalInsertar} toggle={this.ocultarModalInsertar}>
-          <ModalHeader toggle={this.ocultarModalInsertar}>Registrar conductor</ModalHeader>
+        <Modal isOpen={modalInsertar}>
+          <ModalHeader>
+            <div>
+              <h3>Agregar Conductor</h3>
+            </div>
+          </ModalHeader>
+
           <ModalBody>
             <FormGroup>
-              <label>Id:</label>
-              <input type="text" className="form-control" name="id" value={this.state.form.id} onChange={this.handleChange} required />
-            </FormGroup>
-
-            <FormGroup>
               <label>Nombre:</label>
-              <input type="text" className="form-control" name="nombre" value={this.state.form.nombre} onChange={this.handleChange} required />
+              <input
+                className="form-control"
+                type="text"
+                name="nombre"
+                value={form.nombre}
+                onChange={this.handleChange}
+              />
             </FormGroup>
 
             <FormGroup>
               <label>Apellidos:</label>
-              <input type="text" className="form-control" name="apellidos" value={this.state.form.apellidos} onChange={this.handleChange} required />
+              <input
+                className="form-control"
+                type="text"
+                name="apellidos"
+                value={form.apellidos}
+                onChange={this.handleChange}
+              />
             </FormGroup>
 
             <FormGroup>
               <label>Cédula:</label>
-              <input type="text" className="form-control" name="cedula" value={this.state.form.cedula} onChange={this.handleChange} required />
+              <input
+                className="form-control"
+                type="text"
+                name="cedula"
+                value={form.cedula}
+                onChange={this.handleChange}
+              />
             </FormGroup>
 
             <FormGroup>
               <label>Correo:</label>
-              <input type="email" className="form-control" name="correo" value={this.state.form.correo} onChange={this.handleChange} required />
+              <input
+                className="form-control"
+                type="email"
+                name="correo"
+                value={form.correo}
+                onChange={this.handleChange}
+              />
             </FormGroup>
 
             <FormGroup>
               <label>Dirección:</label>
-              <input type="text" className="form-control" name="direccion" value={this.state.form.direccion} onChange={this.handleChange} required />
+              <input
+                className="form-control"
+                type="text"
+                name="direccion"
+                value={form.direccion}
+                onChange={this.handleChange}
+              />
             </FormGroup>
 
             <FormGroup>
               <label>Sexo:</label>
-              <select className="form-control" name="sexo" value={this.state.form.sexo} onChange={this.handleChange} required>
+              <select
+                className="form-control"
+                name="sexo"
+                value={form.sexo}
+                onChange={this.handleChange}
+              >
                 <option value="">Seleccione...</option>
                 <option value="M">Masculino</option>
                 <option value="F">Femenino</option>
@@ -250,22 +288,56 @@ class Conductor extends Component {
                 <option value="20">Tunja</option>
               </select>
             </FormGroup>
+            {this.state.mostrarAlerta && (
+              <div className="alert alert-warning" role="alert">
+                Por favor complete todos los campos.
+              </div>
+            )}
+
           </ModalBody>
 
           <ModalFooter>
-            {this.state.conductorSeleccionado ? (
-              <Button color="primary" onClick={() => this.actualizar()}>Actualizar</Button>
+            {form.id ? (
+              <Button color="primary" onClick={this.editarConductor}>
+                Editar
+              </Button>
             ) : (
-              <Button color="primary" onClick={() => this.insertar()}>Insertar</Button>
+              <Button color="primary" onClick={this.insertarConductor}>
+                Insertar
+              </Button>
             )}
-            <Button color="danger" onClick={() => this.ocultarModalInsertar()}>Cancelar</Button>
+            {' '}
+            <Button color="danger" onClick={this.ocultarModalInsertar}>
+              Cancelar
+            </Button>
           </ModalFooter>
         </Modal>
-      </>
+
+        <Modal isOpen={mostrarAlerta}>
+          <ModalHeader>
+            <div>
+              <h3>Eliminar Conductor</h3>
+            </div>
+          </ModalHeader>
+
+          <ModalBody>
+         ¿Está seguro de que desea eliminar al conductor {conductorAEliminar?.nombre}?
+
+          </ModalBody>
+
+          <ModalFooter>
+            <Button color="danger" onClick={this.eliminarConductor}>
+              Eliminar
+            </Button>
+            {' '}
+            <Button color="secondary" onClick={this.ocultarModalEliminar}>
+              Cancelar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </Container>
     );
   }
 }
 
 export default Conductor;
-
-
